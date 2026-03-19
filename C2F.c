0d87beea589f7c95e32d3bc803603edafa90cf9a
V R Dhanush Raj <vrdhanushraj@gmail.com>
2026-03-19 09:00:47 +0530

    Typo: C2F is repeated 2 times

--- C2F.c
+++ C2F.c
@@ -6,25 +6,25 @@ float fahrenheit;
 
 void c2f(float celsius){
 	float conv=celsius*9/5+32;
-	printf("your celsius temperature of %2f is %2f in fahrenheit", celsius, conv);
+	printf("\nYour celsius temperature of %2f is %2f in fahrenheit", celsius, conv);
 }
 void f2c(float fahrenheit){
 	float conv=(fahrenheit-32)*5/9;
-	printf("your fahrenheit temperature of %2f is %2f in celsius", fahrenheit, conv);
+	printf("\nYour fahrenheit temperature of %2f is %2f in celsius", fahrenheit, conv);
 }
 void main(void){
 	int inputF;
-	printf("would you like to convert from celsius to fahrenheit or celsius to fahrenheit?\n");
+	printf("Would you like to convert from celsius to fahrenheit or fahrenheit to celsius?\n");
 	printf("[1]C2F\n[2]F2C\n");
-	scanf("%d",&inputF);
+	scanf("%d", &inputF);
 	switch (inputF){
 		case 1:
-			printf("type in the celsius value::");
+			printf("\nType in the celsius value: ");
 			scanf("%2f",&inputA);
 			c2f(inputA);
 			break;
 		case 2:
-			printf("type in the fahrenheit value::");
+			printf("\nType in the fahrenheit value: ");
 			scanf("%2f", &inputA);
 			f2c(inputA);
 			break;
